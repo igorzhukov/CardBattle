c86e327f7451eaa05859d46edc64fa6909928dc4
Igor Zhukov <Gar@MacBook-Pro-Igor.local>
2017-01-10 18:20:26 +0200

    Initial Commit

---
+++ CardBattle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardBattle
+//
+//  Created by Igor Zhukov on 1/10/17.
+//  Copyright © 2017 Igor Zhukov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
