6441c8513898ca769c7f9bb217653d33bf1f24a8
Igor Zhukov <Gar@MacBook-Pro-Igor.local>
2017-01-17 17:58:08 +0200

    UI elements were hooked to ViewController. Adding of the cards array. Randomizing for random card extraction.

--- CardBattle/ViewController.swift
+++ CardBattle/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cpuImageView: UIImageView!
+    @IBOutlet weak var playerImageView: UIImageView!
+    @IBOutlet weak var playerScoreLabel: UILabel!
+    @IBOutlet weak var cpuScoreLabel: UILabel!
+    
+    let cardsArray = ["ace", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jcak", "queen", "king"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dealTapped(_ sender: UIButton) {
+        
+        var playerRandomNumber = arc4random_uniform(13)
+        playerImageView.image = UIImage(named: cardsArray[palyerRandomNumber])
+        
+    }
+    
 
 }
 
