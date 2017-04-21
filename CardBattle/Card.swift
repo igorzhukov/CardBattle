cd325ec2310176439046732625e9af559f6baa68
Igor Zhukov <Gar@macbook-pro-igor.local>
2017-04-21 14:02:13 +0300

    getting assetName from arrayOfCardType and display card on UIView

---
+++ CardBattle/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  CardBattle
+//
+//  Created by Igor Zhukov on 4/20/17.
+//  Copyright © 2017 Igor Zhukov. All rights reserved.
+//
+
+import UIKit
+
+class Card {
+    var name: String?
+    var value: Int?
+}
--- CardBattle/ViewController.swift
+++ CardBattle/ViewController.swift
@@ -19,42 +19,65 @@ class ViewController: UIViewController {
     @IBOutlet weak var cpuScoreLabel: UILabel!
     var cpuScore = 0
     
+    let cardsNamesArray = ["card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king", "ace"]
+    
+    var cardsArray = [Card]()
     
     
-    
-    let cardsArray = ["card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king", "ace"]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    func cardsInit(from arrayWithCardsNames: [String]) {
+        for (index, cardName) in cardsNamesArray.enumerated()  {
+            let card = Card()
+            card.name = cardName
+            card.value = index
+            self.cardsArray.append(card)
+        }
     }
-    
-    
-    @IBAction func dealTapped(_ sender: UIButton) {
         
-        var arrayWithRandomNumber = [0, 0]
         
-        for i in 1...2 {
-            arrayWithRandomNumber[i-1] = Int(arc4random_uniform(13))
+        
+        
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // cardsArray init for further parsing
+            cardsInit(from: cardsNamesArray)
+            
         }
         
-        playerImageView.image = UIImage(named: cardsArray[arrayWithRandomNumber[0]])
         
-        cpuImageView.image = UIImage(named: cardsArray[arrayWithRandomNumber[1]])
-        
-        
-        // Compare the card numbers
-        if arrayWithRandomNumber[0] > arrayWithRandomNumber[1] {
-            playerScore += 1
-            playerScoreLabel.text = String(playerScore)
-        } else if arrayWithRandomNumber[0] == arrayWithRandomNumber[1] {
-            //nothing to do in that case
-        } else {
-            cpuScore += 1
-            cpuScoreLabel.text = String(cpuScore)
+        @IBAction func dealTapped(_ sender: UIButton) {
+            
+            var arrayWithRandomNumber = [0, 0]
+            
+            for i in 0...1 {
+                arrayWithRandomNumber[i] = Int(arc4random_uniform(13))
+            }
+            
+            let playerImageName = UIImage(named: cardsArray[arrayWithRandomNumber[0]].name!)
+            playerImageView.image = playerImageName
+            
+            let cpuImageName = UIImage(named: cardsArray[arrayWithRandomNumber[1]].name!)
+            cpuImageView.image = cpuImageName
+            
+            
+
+            
+            
+//            // Compare the card numbers
+//            if arrayWithRandomNumber[0] > arrayWithRandomNumber[1] {
+//                playerScore += 1
+//                playerScoreLabel.text = String(playerScore)
+//            } else if arrayWithRandomNumber[0] == arrayWithRandomNumber[1] {
+//                //nothing to do in that case
+//            } else {
+//                cpuScore += 1
+//                cpuScoreLabel.text = String(cpuScore)
+//            }
+            
         }
         
-    }
-    
-    
+        
 }
 
